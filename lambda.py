cac930a539c20087527cc893f45cbb69e10bc433
Tsyriulnikov <max_djvu@mail.ru>
2023-02-22 14:59:15 +0300

    import

--- lambda.py
+++ lambda.py
@@ -1,3 +1,4 @@
+import math
 # filter
 my_list = [1, 5, 7, 8, 9, 10]
 new_list = list(filter(lambda x:(x%2==0),my_list))
@@ -6,3 +7,6 @@ print(new_list)
 # map
 new_list = list(map(lambda x:(x*2),my_list))
 print(new_list)
+
+print(dir(math))
+print(math.__loader__)
