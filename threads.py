2b4a839dd99c9737bc592c466abc5958aea3b9cf
Tsyriulnikov <max_djvu@mail.ru>
2023-03-01 14:56:28 +0300

    threads

--- threads.py
+++ threads.py
@@ -1,3 +1,17 @@
 from time import sleep, perf_counter
 from threading import Thread
 
+def task(id):
+    print(f'Start task {id}...')
+    sleep(1)
+    print(f'Task {id} finished')
+
+start_time = perf_counter()
+
+# create and run 10 threads
+threads = []
+for n in range(1,11):
+    t = Thread(target=task, args=(n,))
+    threads.append(t)
+    t.start()
+#
